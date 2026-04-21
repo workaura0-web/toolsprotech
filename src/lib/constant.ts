dc4d9e55e5012082c4b0826c54d822faac64bd14
workaura0-web <workaura0@gmail.com>
2026-04-21 11:04:43 +0500

    xd

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,6 +1,6 @@
 export const SITE_URL = 'https://toolsprotech.com'
 export const SITE_NAME = 'ToolsProTech'
-export const SITE_TITLE = 'ToolsProTech - Free Online Tools'
+export const SITE_TITLE = 'ToolsProTech - Free Online Tools By AB-Meraj'
 export const SITE_DESCRIPTION = 'Free online tools for various tasks without losing quality. ToolsProTech is the best tool for your needs.'
 export const SITE_AUTHOR = 'AB-Meraj'
 export const SITE_EMAIL = "workaura0@gmail.com"
